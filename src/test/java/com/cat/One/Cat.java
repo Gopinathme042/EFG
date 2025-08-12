fd453f6360f1b05bd453bf5204aaf6c6d2efc52a
Gopinathme042 <gopinath.elumalai@speridian.com>
2025-08-12 09:06:39 +0530

    TC

--- src/test/java/com/cat/One/Cat.java
+++ src/test/java/com/cat/One/Cat.java
@@ -4,8 +4,7 @@ public class Cat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Cat");
-System.out.println("Cat");
+		System.out.println("Cat");
+		System.out.println("Cat");
 	}
-
 }
